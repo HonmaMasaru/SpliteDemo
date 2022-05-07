c120e0b325b723e2f7319bfedd048e8115dac165
Honma Masaru <msr400r@gmail.com>
2022-05-07 16:43:55 +0900

    - コメントの整備 - リファクタリング

--- SpliteDemo/GameScene.swift
+++ SpliteDemo/GameScene.swift
@@ -8,47 +8,72 @@
 import SpriteKit
 
 final class GameScene: SKScene {
-    private let colors: [UIColor] = [.red, .green, .blue, .cyan, .magenta, .yellow]
+    /// カラースキーム
+    private let colors: [UIColor] = [
+        .init(red: 140.0 / 255.0, green:   3.0 / 255.0, blue:  39.0 / 255.0, alpha: 1),
+        .init(red: 217.0 / 255.0, green:  85.0 / 255.0, blue: 133.0 / 255.0, alpha: 1),
+        .init(red:  75.0 / 255.0, green: 137.0 / 255.0, blue: 191.0 / 255.0, alpha: 1),
+        .init(red:  65.0 / 255.0, green: 119.0 / 255.0, blue: 166.0 / 255.0, alpha: 1),
+        .init(red: 166.0 / 255.0, green: 155.0 / 255.0, blue: 124.0 / 255.0, alpha: 1)
+    ]
+
+    /// 円のサイズ (ランダムの範囲)
+    private let nodeSize: ClosedRange<CGFloat> = 10...30
+
+    /// インデックス
     private var i: UInt32 = 1
+
+    /// クリックされたポジションのバッファー
     private var newPosition: CGPoint = .zero
 
+    /// https://developer.apple.com/documentation/spritekit/skscene/1519607-didmove
+    /// - Parameter view: ビュー
     override func didMove(to view: SKView) {
         setEdge()
     }
 
+    /// https://developer.apple.com/documentation/uikit/uiresponder/1621084-touchesended
+    /// - Parameters:
+    ///   - touches: タッチ
+    ///   - event: イベント
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         newPosition = touches.first?.location(in: view) ?? .zero
     }
 
+    /// https://developer.apple.com/documentation/spritekit/skscene/1519802-update
+    /// - Parameter currentTime: システムタイム
     override func update(_ currentTime: TimeInterval) {
-        if newPosition != .zero {
-            view?.scene?.addChild(makeCircle(mask: i, position: newPosition))
-            newPosition = .zero
-            i += 1
-        }
+        guard newPosition != .zero else { return }
+        view?.scene?.addChild(makeCircle(position: newPosition))
+        newPosition = .zero
+        i += 1
     }
 
+    /// シーンのリセット
     func reset() {
         view?.scene?.removeAllChildren()
         i = 1
         setEdge()
     }
 
+    /// 境界の設定
     private func setEdge() {
-        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
-        physicsBody?.isDynamic = true
-        physicsBody?.affectedByGravity = false
+        physicsBody = .init(edgeLoopFrom: frame)
         physicsBody?.collisionBitMask = UInt32(0)
     }
 
-    private func makeCircle(mask: UInt32, position: CGPoint) -> SKShapeNode {
-        let size = CGFloat.random(in: 30...80)
+    /// 円の作成
+    /// - Parameters:
+    ///   - mask: マスクID
+    ///   - position: 初期の位置
+    /// - Returns: 円のシェイプ
+    private func makeCircle(position: CGPoint) -> SKShapeNode {
+        let size = CGFloat.random(in: nodeSize)
         let ball = SKShapeNode(circleOfRadius: size)
-        ball.fillColor = (colors.randomElement() ?? .white)
-        ball.position = convertPoint(fromView: position)
-        ball.physicsBody = SKPhysicsBody(circleOfRadius: size)
-        ball.physicsBody?.isDynamic = true
-        ball.physicsBody?.collisionBitMask = mask
+        ball.fillColor = colors.randomElement() ?? .white
+        ball.position = convertPoint(fromView: position) // ポイントの変換 (UIView -> SKScene)
+        ball.physicsBody = .init(circleOfRadius: size)
+        ball.physicsBody?.collisionBitMask = i
         return ball
     }
 }
--- SpliteDemo/GameViewController.swift
+++ SpliteDemo/GameViewController.swift
@@ -9,13 +9,18 @@ import UIKit
 import SpriteKit
 
 final class GameViewController: UIViewController {
+    /// SpliteKitシーンのアウトレット
     @IBOutlet weak var gameView: SKView!
+
+    /// SpliteKitシーン
     private var gameScene: GameScene!
 
+    /// https://developer.apple.com/documentation/uikit/uiviewcontroller/1621495-viewdidload
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    /// https://developer.apple.com/documentation/uikit/uiviewcontroller/1621398-viewdidlayoutsubviews
     override func viewDidLayoutSubviews() {
         gameScene = GameScene(size: view.frame.size)
         gameView.presentScene(gameScene)
@@ -23,14 +28,18 @@ final class GameViewController: UIViewController {
         gameView.showsNodeCount = true
     }
 
+    /// clearボタンのハンドラ
+    /// - Parameter sender: 呼び出し元
     @IBAction func onTapClear(_ sender: UIButton) {
         gameScene.reset()
     }
 
+    /// https://developer.apple.com/documentation/uikit/uiviewcontroller/1621419-shouldautorotate
     override var shouldAutorotate: Bool {
         true
     }
 
+    // https://developer.apple.com/documentation/uikit/uiviewcontroller/1621435-supportedinterfaceorientations
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         UIDevice.current.userInterfaceIdiom == .phone ? .allButUpsideDown : .all
     }
